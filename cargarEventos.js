7d31e269de6ca639fee1355ce58c07af0c39c5b4
rbtickets <rbtickets1409@gmail.com>
2025-04-29 17:03:42 -0600

    detalles de estilo

--- cargarEventos.js
+++ cargarEventos.js
@@ -1,22 +1,22 @@
-function cargarEventos(categoria) {
+function cargarEventos() {
   fetch('eventos.json')
     .then(respuesta => respuesta.json())
     .then(datos => {
       let contenedor = document.querySelector('.container-cards');
-      contenedor.innerHTML = ''; // Limpiar el contenedor antes de agregar los eventos
+      contenedor.innerHTML = ''; 
 
-      let eventosFiltrados = datos.filter(evento => evento.categoria === categoria);
+      let eventos = datos.filter(evento => evento);
 
-      eventosFiltrados.forEach(evento => {
+      eventos.forEach(evento => {
         let tarjeta = document.createElement('div');
         tarjeta.classList.add('card');
-        tarjeta.style.width = '18rem';
+        tarjeta.style.width = '14rem';
         tarjeta.innerHTML = `
           <img src="${evento.imagen}" class="card-img-top" alt="${evento.alt}">
           <div class="card-body">
             <h5 class="card-title"><strong>${evento.titulo}</strong></h5>
             <p class="card-text">${evento.lugar} <br> ${evento.fecha}</p>
-            <a href="${evento.enlaceCompra}" class="btn btn-primary">Comprar</a>
+            <a href="${evento.enlaceCompra}" class="btn btn-primary">Ver Detalles</a>
           </div>
         `;
         contenedor.appendChild(tarjeta);
@@ -24,3 +24,5 @@ function cargarEventos(categoria) {
     })
     .catch(error => console.error('Error al cargar los eventos:', error));
 }
+
+
